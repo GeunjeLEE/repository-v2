f2f633b795160eca54325394157b761500436731
Geunje Lee <glee@megazone.com>
2023-02-13 22:25:37 +0900

    chore: update init and global_conf

---
+++ src/cloudforet/repository/conf/global_conf.py
@@ -0,0 +1,10 @@
+DATABASE_AUTO_CREATE_INDEX = True
+DATABASES = {
+    'default': {
+        'db': 'repository_v2',
+        'host': 'localhost',
+        'port': 27017,
+        'username': 'repository_v2',
+        'password': ''
+    }
+}
