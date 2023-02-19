8752d078e278f51535bd05f526b18b07484c4c54
Geunje Lee <glee@megazone.com>
2023-02-19 11:03:08 +0900

    feat: add sync option validation

--- src/cloudforet/repository/service/provider_service.py
+++ src/cloudforet/repository/service/provider_service.py
@@ -2,34 +2,40 @@ import logging
 
 from spaceone.core.service import *
 from cloudforet.repository.manager.provider_manager import ProviderManager
+from cloudforet.repository.error.provider import *
 
 _LOGGER = logging.getLogger(__name__)
 
 
 class ProviderService(BaseService):
 
-    def __init__(self, metadata):
-        super().__init__(metadata)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.provider_mgr: ProviderManager = self.locator.get_manager(ProviderManager)
 
     @transaction(append_meta={'authorization.scope': 'DOMAIN'})
     @check_required(['provider', 'name', 'domain_id'])
     def create(self, params):
-        """ TODO: Add description
-        Args:
-            params (dict): {
+        params = self._validate_sync_options(params)
 
-            }
+        # TODO: Check using identity, secret api / long term
+        #  'identity.ServiceAccount': 'aws_service_account',
+        #  'secret.TrustedSecret': 'aws_access_key',
+        #  'secret.Secret': ['aws_assume_role', 'aws_access_key']
+        #  if params.get('schema'):
+        #      call manager -> connector -> secret / identity
 
-        Returns:
-            provider_vo (object)
-        """
+        # TODO: How do i validate description?
+        #  if params.get('description'):
+        #      pass
+
         return self.provider_mgr.create_provider(params)
 
     @transaction(append_meta={'authorization.scope': 'DOMAIN'})
     @check_required(['provider', 'domain_id'])
     def update(self, params):
-        return self.provider_mgr.update_provider(params)
+        provider_vo = self.provider_mgr.get_provider(params['provider'], params['domain_id'])
+        return self.provider_mgr.update_provider_by_vo(params, provider_vo)
 
     @transaction(append_meta={'authorization.scope': 'DOMAIN'})
     @check_required(['provider', 'domain_id'])
@@ -54,3 +60,19 @@ class ProviderService(BaseService):
         query = params.get('query', {})
 
         return self.provider_mgr.list_providers(query)
+
+    @staticmethod
+    def _validate_sync_options(params):
+        if not params.get('sync_mode') or params['sync_mode'] == 'NONE':
+            params['sync_options'] = {}
+        else:
+            if not params.get('sync_options'):
+                raise ERROR_INSUFFICIENT_SYNC_OPTIONS(sync_options=params['sync_options'])
+            else:
+                if not params['sync_options'].get('source_type') or not params['sync_options'].get('source'):
+                    raise ERROR_INSUFFICIENT_SYNC_OPTIONS(sync_options=params['sync_options'])
+
+                if params['sync_options']['source_type'] != 'GITHUB':
+                    raise ERROR_INVALID_SOURCE_TYPE(source_type=params['sync_options']['source_type'])
+
+        return params
